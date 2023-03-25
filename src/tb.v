796ad3756458f476ff95fb177cd73f1daf1958c2
Emilian Miron <emilian@slim>
2023-03-25 12:43:00 -0400

    Change templates for Scan Chain RiscV block1.

--- src/tb.v
+++ src/tb.v
@@ -26,7 +26,7 @@ module tb (
     assign segments = outputs[6:0];
 
     // instantiate the DUT
-    seven_segment_seconds seven_segment_seconds(
+    diferential_rvb1 diferential_rvb1(
         `ifdef GL_TEST
             .vccd1( 1'b1),
             .vssd1( 1'b0),
